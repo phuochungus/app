f2ef468559a731c8eb5ddf509fedfbc53a66b311
HungNP <21520252@gm.uit.edu.vn>
2023-12-09 15:22:22 +0700

    enable cors

--- app/server.py
+++ app/server.py
@@ -1,10 +1,22 @@
-from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import RedirectResponse
 from langserve import add_routes
+from fastapi import FastAPI
 
 from rag_redis.chain import chain as rag_redis_chain
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 
 @app.get("/")
